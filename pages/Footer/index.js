aba1561f4efb3d724fbc5cfc393d58bb2f97b515
karlosferreira <desenvolvimento.carlosferreira@gmail.com>
2023-02-04 18:10:26 -0300

    footer menu - init

--- pages/Footer/index.js
+++ pages/Footer/index.js
@@ -21,9 +21,14 @@ export default function Footer() {
                                     <div className="input-group email mb-2">
                                         <input type="email" className="form-control" placeholder="Endereço de Email válido" aria-label="search" aria-describedby="basic-addon1" />
                                     </div>
-                                    <div className="input-group check">
-                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
-                                        <span>Li e concordo com os <a href="#">Termos de Consentimento.*</a></span>
+                                    <div className="input-group row send m-0">
+                                        <div className="col-md-9 col-sm-9 col-xs-9 p-0">
+                                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                                            <span>Li e concordo com os <a href="#">Termos de Consentimento.*</a></span>
+                                        </div>
+                                        <div className="col-md-3 col-sm-3 col-xs-3 p-0 pr-1">
+                                            <button type="submit" class="btn btn-primary">Cadastrar</button>
+                                        </div>
                                     </div>
                                 </form>                                
                             </div>
@@ -56,6 +61,105 @@ export default function Footer() {
                                         </div>
                                     </div>
                                 </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="container">
+                        <div className="row footer-menu">
+                            <div className="col-md-3 col-sm-3 col-xs-3">
+                                <h3>FALE CONOSCO</h3>
+                                <ul>
+                                    <li>
+                                        <a href="#">Acompanhe seu pedido</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Televendas (21) 99363-2769</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Atendimento</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">SAC</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Atendimento a Empresas</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="col-md-3 col-sm-3 col-xs-3">
+                                <h3>AJUDA E SUPORTE</h3>
+                                <ul>
+                                    <li>
+                                        <a href="#">Regulamentos</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Política de Privacidade</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Política de Cookies</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Políticas de Trocas</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Perguntas Frequentes</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Segurança</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Fornecedores</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Serviços</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="col-md-3 col-sm-3 col-xs-3">
+                                <h3>INSTITUCIONAL</h3>
+                                <ul>
+                                    <li>
+                                        <a href="#">Item</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Item</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Item</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Item</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Item</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Item</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="col-md-3 col-sm-3 col-xs-3">
+                                <h3>Menu</h3>
+                                <ul>
+                                    <li>
+                                        <a href="#">Item</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Item</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Item</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Item</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Item</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Item</a>
+                                    </li>
+                                </ul>
                             </div>
                         </div>
                     </div>
